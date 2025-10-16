6e6069b64c917034621af91d4c719f73ea0cc531
Nuva424 <murataka0424@gmail.com>
2025-10-16 09:07:33 +0900

    feat: 長い待機時間のMCPサーバーを検証してみた - 規定のタイムアウト時間で終了してしまう……

--- server.py
+++ server.py
@@ -1,15 +1,43 @@
 # server.py
+import asyncio
+from datetime import datetime
 from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.server import Context
 
 # Create an MCP server with debug enabled
 mcp = FastMCP("Demo", debug=True)
 
 
-# Add an addition tool
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+def get_current_time() -> str:
+    """現在時刻を取得する
+
+    Returns:
+        str: 現在時刻
+    """
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+@mcp.tool()
+async def long_sleep(minutes: int, ctx: Context) -> str:
+    """長時間、待つ。
+
+    Args:
+        minutes: 待つ時間（分）
+        ctx: コンテキスト
+
+    Returns:
+        str: 終わったよ！
+
+    None:
+        場合によってはタイムアウトする可能性がある。そのため、long_sleepを呼び出す前に、get_current_timeを呼び出して、現在時刻を取得しておくことを推奨する。
+    """
+    REPORT_INTERVAL = 30  # 60秒ごとに進捗を報告
+    MAX_COUNT = minutes * 60 // REPORT_INTERVAL
+    for i in range(MAX_COUNT):
+        await ctx.report_progress(i, MAX_COUNT)
+        await asyncio.sleep(REPORT_INTERVAL)
+    return "終わったよ！"
 
 
 # Add this part to run the server
